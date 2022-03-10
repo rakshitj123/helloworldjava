2b194f91f5773ea2d14938b4420ff061c07a2d04
rakshitj123 <79228357+rakshitj123@users.noreply.github.com>
2022-03-10 10:54:02 -0500

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from jenkins k");
+    System.out.println("Hello World from jenkins ji");
     }
 }
 
